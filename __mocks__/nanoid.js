183beb2f6b05f79b4e114af868eda09abe0fccd0
LittleWhale <zverevn420@gmail.com>
2021-03-25 12:46:41 +0300

    compilation artefacts deleted

--- __mocks__/nanoid.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.nanoid = void 0;
-const nanoid = (num) => {
-    return Array(num).join("x");
-};
-exports.nanoid = nanoid;
-//# sourceMappingURL=nanoid.js.map
