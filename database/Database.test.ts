8a2eb2bd0b49e64b836edbb6fc81baef9e8fe33c
LittleWhale <zverevn420@gmail.com>
2021-03-31 19:53:48 +0300

    Add snapshots for authoriseUser test

--- database/Database.test.ts
+++ database/Database.test.ts
@@ -2,22 +2,23 @@ import WishListFileDatabase from "./Database";
 import fs from "fs";
 import tmp from "tmp"
 import path from "path"
-import {IWishListDb} from "./interfaces";
 
 jest.mock('nanoid')
 
+const createEmptyTestDatabase = () => {
+    const dbFilePath = path.join(tmp.dirSync().name, "db.json")
+    return { db: new WishListFileDatabase(dbFilePath), dbFilePath }
+}
 
 describe("createMagicId", () => {
     test("should create and return magic ID", async () => {
-        const dbFilePath = "./data/testDb"
-        const db = new WishListFileDatabase(dbFilePath);
+        const { db } = createEmptyTestDatabase();
         const magicId = await db.createMagicId("farglowparty@gmail.com");
         expect(magicId).toBe("xxxxxxxxxxxxxxxx")
     })
 
     test("should create new user entity it doesn't exist", async () => {
-        const dbFilePath = path.join(tmp.dirSync().name, "db.json")
-        const db = new WishListFileDatabase(dbFilePath);
+        const { db, dbFilePath } = createEmptyTestDatabase();
         await db.createMagicId("farglowparty@gmail.com");
         const fileContent = await fs.promises.readFile(dbFilePath, {
             encoding: "utf-8"
@@ -26,17 +27,24 @@ describe("createMagicId", () => {
     })
 
     // test("should re-create magic id if it exists", () => {
-    //     const dbFilePath = path.join(tmp.dirSync().name, "db.json")
-    //     const db = new WishListFileDatabase(dbFilePath)
+    //     const dbFilePath =createTestDbFile
+    //     const db = createEmptyTestDatabase()
     // })
 })
 
 describe("authoriseUser", () => {
     test("should delete authorise request and create user session", async () => {
-        const dbFilePath = path.join(tmp.dirSync().name, "db.json")
-        const db = new WishListFileDatabase(dbFilePath)
+        const { db, dbFilePath } = createEmptyTestDatabase()
         await db.createMagicId("farglowparty@gmail.com");
+        const fileContentAfterCreatingMagicId = await fs.promises.readFile(dbFilePath, {
+            encoding: "utf-8"
+        });
+        expect(fileContentAfterCreatingMagicId).toMatchSnapshot()
         const cookie = await db.authoriseUser("xxxxxxxxxxxxxxxx")
         expect(cookie).toBe('xxxxxxxxxxxxxxxxxx')
+        const fileContentAfterAuthoriseUser = await fs.promises.readFile(dbFilePath, {
+            encoding: "utf-8"
+        });
+        expect(fileContentAfterAuthoriseUser).toMatchSnapshot()
     })
 })
