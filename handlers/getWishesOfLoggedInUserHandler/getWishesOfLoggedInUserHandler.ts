383804d0f8746fef73baad45f5b92d9b22d7ebad
LittleWhale <zverevn420@gmail.com>
2021-04-05 14:53:22 +0300

    get wishesOfLoggedInUser implemented

---
+++ handlers/getWishesOfLoggedInUserHandler/getWishesOfLoggedInUserHandler.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import WishListFileDatabase from "../../database/Database";
+import {databasePath} from "../../addresses";
+import {IWishRow} from "../../database/interfaces";
+
+const getWishesOfLoggedInUserHandler = (req: express.Request, res: express.Response) => {
+    const cookie = req.cookies['auth-token']
+    console.log(cookie)
+    if (!cookie) {
+        res.status(500).send("Cookie doesn't exist")
+        return
+    }
+    const dbInstance = new WishListFileDatabase(databasePath)
+    dbInstance.getAllWishesOfLoggedInUser(cookie)
+        .then((data: IWishRow[]) => {
+            console.log(data)
+            res.status(200).send(data)
+        })
+        .catch((err: Error) => {
+            console.error(err)
+            res.status(500).send(err)
+        })
+}
+
+export default getWishesOfLoggedInUserHandler
